ba2204c8f3cb92e216c292e33f9b77f2d5de7463
Emir <amirshadanfar@teknasyon.com>
2021-07-12 15:36:43 +0300

    fix some issue in service

---
+++ src/Constants/ServiceAclInfoType.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Suite\Suite\Constants;
+
+class ServiceAclInfoType
+{
+    use CustomModelTrait;
+
+    const BASE_URL = 'base_url';
+    const ACL_ROLES_URL = 'acl_roles_url';
+    const ACL_ROLES_METHOD = 'acl_roles_method';
+}
